dbe261eb8936ceed3b78e4a19783c53c7d9a99f0
Jesús Escribano <jesusescribano2002@gmail.com>
2024-08-10 10:22:41 +0200

    chore: Update config files 🔧

--- tests/e2e/specs/test.js
+++
@@ -1,9 +0,0 @@
-// https://docs.cypress.io/api/table-of-contents
-
-describe("Frontend Tests", () => {
-  it("Visit the homepage", () => {
-    cy.visit("/");
-    cy.contains("h1", "¡Bienvenid@ a VotUCO: El sistema de votaciones on-line de la Universidad de Córdoba!");
-    cy.get("[data-cy='signin-button']").click()
-  });
-});
---
+++ tests/e2e/specs/tests.js
@@ -0,0 +1,12 @@
+// https://docs.cypress.io/api/table-of-contents
+
+describe("Homepage Test", () => {
+  it("Visit the homepage", () => {
+    cy.visit("/");
+    cy.contains(
+      "h1",
+      "¡Bienvenid@ a VotUCO: El sistema de votaciones on-line de la Universidad de Córdoba!"
+    );
+    cy.get("[data-cy='signin-button']").click();
+  });
+});
