4fa24712b1402f5006a51e5b6c140a57d45de366
Jesús Escribano <jesusescribano2002@gmail.com>
2024-08-16 17:24:51 +0200

    chore: Create 404 component 👽️

---
+++ src/components/NotFound.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <img src="@/assets/404.jpeg" alt="Imagen de 404 Beagle" id="perro_img" />
+    <p>
+      No has encontrado lo que buscabas, pero has encontrado a este perrito
+      democrático
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NotFound",
+  setup() {},
+};
+</script>
+
+<style scoped>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10vw;
+}
+
+#perro_img {
+  margin: 50px;
+  width: 15vw;
+}
+
+p {
+  font-size: 2vw;
+  font-weight: bolder;
+  text-align: center;
+}
+</style>
