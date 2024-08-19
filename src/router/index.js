bfcbb8c9761fe90b3f42e4d845d3dc82a66e184e
Jesús Escribano <jesusescribano2002@gmail.com>
2024-08-19 16:25:01 +0200

    build: Update router and localStorage 🔧

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,30 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
-import VotesView from "@/views/VotesView.vue";
 import VotingView from "@/views/VotingView.vue";
 import RegisterUserView from "@/views/RegisterUserView.vue";
 import CreateVotationView from "@/views/CreateVotationView.vue";
 import LogInView from "@/views/LogInView.vue";
+import OAuthLoginRedirect from "@/components/OAuthLoginRedirect.vue";
 //import CreateVotationView from "../views/CreateVotationView.vue";
+import NotFound from "@/components/NotFound.vue";
+import VoteChecker from "@/components/VoteChecker.vue";
+import VotingChecker from "@/components/VotingChecker.vue";
+import ScoringVoteView from "@/views/ScoringVoteView.vue";
+import SchuzleVoteView from "@/views/SchuzleVoteView.vue";
+import PublishVoteView from "@/views/PublishVoteView.vue";
+import ModifyVotingView from "@/views/ModifyVotingView.vue";
+import UserListView from "@/views/UserListView.vue";
+import RegisterFormAdminView from "@/views/RegisterFormAdminView.vue";
 
 const routes = [
   {
     path: "/",
     name: "home",
     component: HomeView,
+    meta: {
+      authRequired: false,
+      adminRequired: false,
+    },
   },
   //{
   //  path: "/about",
@@ -23,19 +36,31 @@ const routes = [
   //    import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   //},
   {
-    path: "/votes2",
-    name: "votes",
-    component: VotesView,
-  },
-  {
     path: "/voting",
     name: "votes",
     component: VotingView,
+    meta: {
+      authRequired: true,
+      adminRequired: false,
+    },
   },
   {
     path: "/createVote",
     name: "createVote",
     component: CreateVotationView,
+    meta: {
+      authRequired: true,
+      adminRequired: true,
+    },
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: RegisterUserView,
+    meta: {
+      authRequired: false,
+      adminRequired: false,
+    },
   },
   {
     path: "/login",
@@ -43,18 +68,105 @@ const routes = [
     component: LogInView,
     meta: {
       hideNavbar: true,
+      authRequired: false,
+      adminRequired: false,
     },
   },
   {
-    path: "/register2",
-    name: "register",
-    component: RegisterUserView,
+    path: "/login/oauth",
+    name: "loginOAuth",
+    component: OAuthLoginRedirect,
+    meta: {
+      authRequired: false,
+      adminRequired: false,
+    },
   },
+  {
+    path: "/vote/checker",
+    name: "voteChecker",
+    component: VoteChecker,
+    meta: {
+      authRequired: true,
+      adminRequired: false,
+    },
+  },
+  {
+    path: "/voting/checker",
+    name: "votingChecker",
+    component: VotingChecker,
+    meta: {
+      authRequired: true,
+      adminRequired: false,
+    },
+  },
+  {
+    path: "/vote/scoring",
+    name: "scoringVote",
+    component: ScoringVoteView,
+    meta: {
+      authRequired: true,
+      adminRequired: false,
+    },
+  },
+  {
+    path: "/vote/schuzle",
+    name: "schuzleVote",
+    component: SchuzleVoteView,
+    meta: {
+      authRequired: true,
+      adminRequired: false,
+    },
+  },
+  {
+    path: "/publish",
+    name: "publishVote",
+    component: PublishVoteView,
+  },
+  {
+    path: "/modify",
+    name: "modifyView",
+    component: ModifyVotingView,
+  },
+  {
+    path: "/adminUsers",
+    name: "usersList",
+    component: UserListView,
+  },
+  {
+    path: "/admin/createUser",
+    name: "createUserAdmin",
+    component: RegisterFormAdminView,
+    meta: {
+      authRequired: false,
+      adminRequired: true,
+    },
+  },
+  { path: "/:pathMatch(.*)*", name: "404erroe", component: NotFound },
 ];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+});
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.authRequired === true) {
+    if (localStorage.getItem("accessToken") !== "null") {
+      if (to.meta.adminRequired === true) {
+        if (localStorage.getItem("rol") === "admin") {
+          return next();
+        } else {
+          router.push("/404");
+        }
+      } else {
+        return next();
+      }
+    } else {
+      router.push("/404");
+    }
+  } else {
+    return next();
+  }
 });
 
 export default router;
--- src/store/index.js
+++ src/store/index.js
@@ -5,33 +5,49 @@ import { createStore } from "vuex";
 export default createStore({
   state: {
     logged: false,
-    accessToken: null,
+    accessToken: "null",
     refreshToken: null,
+    rol: null,
+    name: null,
   },
   getters: {
-    getRefreshToken(state){
+    getRefreshToken(state) {
       return state.refreshToken;
     },
-    getLogged(state){
+    getLogged(state) {
       return state.logged;
     },
-    getAccessToken(state){
+    getAccessToken(state) {
       return state.accessToken;
-    }
+    },
+    getRol(state) {
+      return state.rol;
+    },
+    getName(state) {
+      return state.name;
+    },
   },
   mutations: {
-    setRefreshToken(state, token){ 
+    setRefreshToken(state, token) {
       state.token = token;
       return state.token;
     },
-    setLogged(state, value){
+    setLogged(state, value) {
       state.logged = value;
       return state.logged;
     },
-    setAccessToken(state, value){
+    setAccessToken(state, value) {
       state.accessToken = value;
       return state.accessToken;
-    }
+    },
+    setRol(state, value) {
+      state.rol = value;
+      return state.rol;
+    },
+    setName(state, value) {
+      state.name = value;
+      return state.name;
+    },
   },
   actions: {},
   modules: {},
